e7c8b28cd3ad60879d1a90c04c1d7b109692e420
arsalanlal414 <arsalanlal414@gmail.com>
2023-10-18 09:22:00 +0500

    admin dashboard

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>ArsalanDashboard</span>
+      <span>© Arsalan Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
